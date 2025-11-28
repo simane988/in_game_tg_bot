3658eb32d271708b5aa0726860db058003e522a6
simane988 <simane988@gmail.com>
2025-11-28 10:28:05 +0300

    Fixed steam integration

--- bot.py
+++ bot.py
@@ -9,8 +9,9 @@ from telegram import Bot
 from telegram.error import TelegramError
 
 # Конфигурация
-TELEGRAM_BOT_TOKEN = os.getenv('TARGET_BOT_TOKEN')
-CHAT_ID = os.getenv('TARGET_CHAT_ID')
+TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+TARGET_CHAT_ID = os.getenv('TARGET_CHAT_ID')
+STEAM_API_KEY = os.getenv('STEAM_API_KEY')
 
 # Список отслеживаемых пользователей steam
 STEAM_IDS = {
@@ -20,14 +21,14 @@ STEAM_IDS = {
 }
 
 # Последние известные игры
-last_known_games = {
+LAST_KNOWN_STEAM_GAMES = {
     'Семён': None,
     'Илья':None,
     'Эмиль': None
 }
 
 # Соответствие имени и тега в telegram
-telegram_ids = {
+TELEGRAM_IDS = {
     'Семён': '396770433',
     'Илья': '1380077865',
     'Эмиль': '704174263'
@@ -35,12 +36,11 @@ telegram_ids = {
 
 
 def get_steam_user_games(steam_id):
-    url = f'https://steamcommunity.com/profiles/{steam_id}?xml=1'
+    url = f'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={STEAM_API_KEY}&steamids={steam_id}'
     try:
-        response = requests.get(url, timeout=10, headers={'Cache-Control': 'no-cache'})
-        cur_game = re.search(r'(?<=In-Game<br\/>).*(?=\]\])', response.text)
-        if cur_game:
-            return cur_game.group(0)
+        response = requests.get(url, timeout=10).json()
+        if 'gameextrainfo' in response['response']['players'][0]:
+            return response['response']['players'][0]['gameextrainfo']
         else:
             return None
     except Exception as e:
@@ -52,7 +52,7 @@ def send_telegram_message(message):
     """Отправить сообщение в Telegram чат"""
     try:
         bot = Bot(token=TELEGRAM_BOT_TOKEN)
-        asyncio.run(bot.send_message(chat_id=CHAT_ID, text=message, parse_mode=telegram.constants.ParseMode.MARKDOWN))
+        asyncio.run(bot.send_message(chat_id=TARGET_CHAT_ID, text=message, parse_mode=telegram.constants.ParseMode.MARKDOWN))
     except TelegramError as e:
         print(f"Ошибка отправки в Telegram: {e}")
 
@@ -62,19 +62,19 @@ def check_activities():
     for name, steam_id in STEAM_IDS.items():
         current_game = get_steam_user_games(steam_id)
         print(f"{name} steam_id: {steam_id}, game: {current_game}")
-        previous_game = last_known_games[name]
+        previous_game = LAST_KNOWN_STEAM_GAMES[name]
 
         if current_game != previous_game:
             if current_game:
                 send_telegram_message(
-                    f"🎮 [{name}](tg://user?id={telegram_ids[name]}) начал играть в _{current_game}_",
+                    f"🎮 [{name}](tg://user?id={TELEGRAM_IDS[name]}) начал играть в _{current_game}_",
                 )
             elif previous_game:
                 send_telegram_message(
-                    f"❌ [{name}](tg://user?id={telegram_ids[name]}) вышел из игры _{previous_game}_"
+                    f"❌ [{name}](tg://user?id={TELEGRAM_IDS[name]}) вышел из игры _{previous_game}_"
                 )
 
-            last_known_games[name] = current_game
+            LAST_KNOWN_STEAM_GAMES[name] = current_game
 
         time.sleep(1)  # Задержка между запросами к API
 
